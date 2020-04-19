a286cb829973ef91726005cdb5b2a652026c34e8
makrau <iago006@hotmail.com>
2020-04-19 19:42:33 -0300

    implementada funcionalidade de like

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
 import {
   SafeAreaView,
@@ -10,46 +10,79 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api';
+
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    await api.post(`repositories/${id}/like`)
+    const updatedRepositories = repositories.map(repository => {
+      if (repository.id === id) {
+        repository.likes += 1;
+      }
+
+      return repository;
+    })
+    setRepositories(updatedRepositories);
+  }
+
+  function renderLikeButton(repository) {
+    return (
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => handleLikeRepository(repository.id)}
+        testID={`like-button-${repository.id}`}
+      >
+        <Text style={styles.buttonText}>Curtir</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  function renderLikesContainer(repository) {
+    return (
+      <View style={styles.likesContainer}>
+        <Text
+          style={styles.likeText}
+          testID={`repository-likes-${repository.id}`}
+        >
+          {repository.likes} curtidas
+        </Text>
+      </View>
+    )
+  }
+
+  function renderRepository({item: repository}) {
+    return (
+      <>
+        <Text style={styles.repository}>{repository.title}</Text>
+        <View style={styles.techsContainer}>
+          {repository.techs.map(tech => (
+            <Text style={styles.tech} key={tech}>{tech}</Text>)
+          )}
+        </View>
+        {renderLikesContainer(repository)}
+        {renderLikeButton(repository)}
+      </>
+    )
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+        <FlatList
+          style={styles.repositoryContainer}
+          data={repositories}
+          keyExtractor={(repository) => repository.id}
+          renderItem={renderRepository}
+        />
       </SafeAreaView>
     </>
   );
@@ -61,7 +94,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#7159c1",
   },
   repositoryContainer: {
-    marginBottom: 15,
+    marginBottom: 10,
     marginHorizontal: 15,
     backgroundColor: "#fff",
     padding: 20,
@@ -69,6 +102,7 @@ const styles = StyleSheet.create({
   repository: {
     fontSize: 32,
     fontWeight: "bold",
+    marginBottom: 5,
   },
   techsContainer: {
     flexDirection: "row",
@@ -95,6 +129,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10,
+    marginBottom: 15,
   },
   buttonText: {
     fontSize: 14,
